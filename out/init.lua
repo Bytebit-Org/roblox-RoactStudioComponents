3b6073498352f86056047b46eebdf8cd9f7dff54
Noah Crowley <noahwillcrow@gmail.com>
2019-10-18 19:59:44 -0700

    Trying this instead

--- out/init.lua
+++ out/init.lua
@@ -1,9 +1,13 @@
 -- Compiled with https://roblox-ts.github.io v0.2.14
--- October 18, 2019, 7:49 PM Pacific Daylight Time
+-- October 18, 2019, 7:58 PM Pacific Daylight Time
 
 local TS = _G[script];
 local exports = {};
-local StudioDialogFrame = TS.import(script, "Components", "StudioDialogFrame");
-local StudioFrame = TS.import(script, "Components", "StudioFrame");
-exports.StudioDialogFrame, exports.StudioFrame = StudioDialogFrame, StudioFrame;
+exports.StudioDialogFrame = TS.import(script, "Components", "StudioDialogFrame").StudioDialogFrame;
+exports.StudioFrame = TS.import(script, "Components", "StudioFrame").StudioFrame;
+exports.StudioImageButton = TS.import(script, "Components", "StudioImageButton").StudioImageButton;
+exports.StudioTextBox = TS.import(script, "Components", "StudioTextBox").StudioTextBox;
+exports.StudioTextButton = TS.import(script, "Components", "StudioTextButton").StudioTextButton;
+exports.StudioTextLabel = TS.import(script, "Components", "StudioTextLabel").StudioTextLabel;
+exports.StudioToggle = TS.import(script, "Components", "StudioToggle").StudioToggle;
 return exports;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-import StudioDialogFrame = require("./Components/StudioDialogFrame");
-import StudioFrame = require("./Components/StudioFrame");
-import StudioImageButton = require("./Components/StudioImageButton");
-import StudioTextBox = require("./Components/StudioTextBox");
-import StudioTextButton = require("./Components/StudioTextButton");
-import StudioTextLabel = require("./Components/StudioTextLabel");
-import StudioToggle = require("./Components/StudioToggle");
-
-export {
-	StudioDialogFrame,
-	StudioFrame,
-	StudioImageButton,
-	StudioTextBox,
-	StudioTextButton,
-	StudioTextLabel,
-	StudioToggle	
-};
+export { StudioDialogFrame } from "./Components/StudioDialogFrame";
+export { StudioFrame } from "./Components/StudioFrame";
+export { StudioImageButton } from "./Components/StudioImageButton";
+export { StudioTextBox } from "./Components/StudioTextBox";
+export { StudioTextButton } from "./Components/StudioTextButton";
+export { StudioTextLabel } from "./Components/StudioTextLabel";
+export { StudioToggle } from "./Components/StudioToggle";
